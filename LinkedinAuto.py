5353e95cb904f6b5dd8adb9f5b68fcbda11058da
Ng <xng@MPKL1130dd9c5.local>
2013-11-03 22:43:29 -0800

    First commit

---
+++ LinkedinAuto.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from pprint import pprint
+from time import gmtime, strftime
+
+#https://api.linkedin.com/v1/companies/1666:(id,num-followers)
+#?oauth2_access_token=AQXqWy92Jtz6qa-qE5DazfVCNBHOI_4uy9abBqUuyagITGt1xFBTQg3BHkpUsXdO2s-3zmAXVj1XjuBzAEm-3Nudyyw-R9n4g8vOOza5evu_CMBzczb__-P3OGCDU2EpjHXkT4fClkiTle__UBEH5oL6FVfoG-pW2cpC81M_FNImZtNWCqs
+payload={
+		 'format':'json',		 
+		 'oauth2_access_token':'AQXqWy92Jtz6qa-qE5DazfVCNBHOI_4uy9abBqUuyagITGt1xFBTQg3BHkpUsXdO2s-3zmAXVj1XjuBzAEm-3Nudyyw-R9n4g8vOOza5evu_CMBzczb__-P3OGCDU2EpjHXkT4fClkiTle__UBEH5oL6FVfoG-pW2cpC81M_FNImZtNWCqs',		 
+		 }
+
+r = requests.get("https://api.linkedin.com/v1/companies/1666:(id,num-followers)", params=payload)
+
+#print r.text
+
+data = json.loads(r.text)
+
+#pprint(data)
+print strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ": " + str(data["numFollowers"])
+
+# print "FROM HERE IS JENN"
+
+# print data["_count"]
+
+# print data["values"][0]
+# print "\n"
+# print data["values"][1]
+# print "\n"
+# print data["values"][3]
+# print "\n"
+# print data["values"][4]
